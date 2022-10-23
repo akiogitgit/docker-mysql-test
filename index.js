79fda3f6830cd0c0de7aaff0d58c128e1340d4ef
akiogitgit <akimaron2828@icloud.com>
2022-10-23 23:54:27 +0900

    feat: node.jsとmysqlを接続して、レコードを画面に表示

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const http = require("http");
+const mysql = require("mysql2");
+const connection = mysql.createConnection({
+  host: "db",
+  user: "root",
+  password: "password",
+  database: "sample"
+});
 
 const hostname = "0.0.0.0";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello DockerCompose!");
+  connection.query(
+    'SELECT id FROM `users`',
+    function (err, results, fields) {
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/plain");
+      if (!results) {
+        res.write("ないよ")
+      }else{
+        results.forEach(result => {
+          res.write(`id: ${result["id"]}\n`);
+        });
+      }
+      res.end();
+    }
+  )
+  // res.statusCode = 200;
+  // res.setHeader("Content-Type", "text/plain");
+  // res.end("Hello DockerCompose!");
 })
 
 server.listen(port, hostname, () => {
